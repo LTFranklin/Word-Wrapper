5456cf262548697ccd88272d9e0aba767a8180e5
LTFranklin <louis.t.franklin@gmail.com>
2017-03-11 21:38:26 +0000

    First Push
    Provided Code

---
+++ PageComposition/BusinessLogic/PageInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic {
+
+  public enum Format { Fill, FillSoft, FillAdjust, LineMoment, FillSet };
+
+  public class PageInput {
+
+    public Format format;
+
+    public int wrap = 0;
+
+    public int wrapSoft = 0;
+
+    public int columnMoment = 0;
+
+    public List<String> words;
+
+    public PageInput() {
+      words = new List<String>();
+    }
+
+    public PageInput(Format format, int wrap, int wrapSoft, int columnMoment, List<String> words) {
+      this.format = format;
+      this.wrap = wrap;
+      this.wrapSoft = wrapSoft;
+      this.columnMoment = columnMoment;
+      this.words = words;
+    }
+
+    public Page Compose() {
+      switch (format) {
+        case Format.Fill: {
+            FillPage page = new FillPage(wrap);
+            page.Add(words);
+            return page;
+          }
+        case Format.FillSoft: {
+            return null;
+          }
+        default: {
+            throw new Exception("Unknown format.");
+          }
+      }
+    }
+  }
+
+}
---
+++ ReadMe.txt
@@ -0,0 +1,34 @@
+-----------
+Definitions
+-----------
+
+----
+Word
+----
+LowerCase
+Must contain 1 vowel
+	2 or more if the length is greater then 3
+vowels appear in alphabetical order
+
+----
+Line
+----
+All lines end with /n
+Length is the sum of the characters
+
+------
+Column
+------
+Position of a character on the line
+
+-------
+Methods
+-------
+
+----
+Fill
+----
+Same order as the input
+Spaces between words on the same line
+Lines cannot be longer then the wrap column
+	Unless a word's length goes over the wrap alone
